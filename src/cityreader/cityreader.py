da2969d29e1d19c38c50b2f24bca10be71dc82f5
Neel Mazumdar <neelmazumdar7@gmail.com>
2020-06-12 21:03:46 -0400

    finished, 1 error remaining

--- src/cityreader/cityreader.py
+++ src/cityreader/cityreader.py
@@ -1,6 +1,16 @@
 # Create a class to hold a city location. Call the class "City". It should have
 # fields for name, lat and lon (representing latitude and longitude).
 
+import csv
+
+class City:
+  def __init__(self, name, lat, lon):
+    self.name = name
+    self.lat = lat
+    self.lon = lon
+
+  def __str__(self):
+    return f"{self.name}, {self.lat}, {self.lon}"
 
 # We have a collection of US cities with population over 750,000 stored in the
 # file "cities.csv". (CSV stands for "comma-separated values".)
@@ -20,6 +30,11 @@ def cityreader(cities=[]):
   # TODO Implement the functionality to read from the 'cities.csv' file
   # For each city record, create a new City instance and add it to the 
   # `cities` list
+  with open('src/cityreader/cities.csv', newline='') as csvfile:
+    data = csv.DictReader(csvfile)
+    for row in data:
+        city = City(f"{row['city']}", float(row['lat']), float(row['lng']))
+        cities.append(city)
     
     return cities
 
@@ -60,10 +75,17 @@ for c in cities:
 
 # TODO Get latitude and longitude values from the user
 
+# lat1 = input('Please enter the latitude of the first point: ')
+# lon1 = input('Please enter the longitude of the first point: ')
+# lat2 = input('Please enter the latitude of the second point: ')
+# lon2 = input('Please enter the longitude of the second point: ')
+
+
 def cityreader_stretch(lat1, lon1, lat2, lon2, cities=[]):
   # within will hold the cities that fall within the specified region
   within = []
-
+  # for city in cities:
+  #   if city.lat <= max(lat1, lat2) and city.lat >= min(lat1, lat2)
   # TODO Ensure that the lat and lon valuse are all floats
   # Go through each city and check to see if it falls within 
   # the specified coordinates.
